79ba5486e542ff5e1c40e8fc5d651ed5f3fef254
seojiwoo23 <seozziu.o@gmail.com>
2026-05-18 12:43:17 +0900

    feat: 토큰 삭제

--- lib/core/env/env.dart
+++ lib/core/env/env.dart
@@ -21,13 +21,13 @@ class Env {
   /// 카카오 네이티브 앱 키.
   static const String kakaoNativeKey = String.fromEnvironment(
     'KAKAO_NATIVE_KEY',
-    defaultValue: '9eb912137d7833c4bda844bebcce1a3c',
+    defaultValue: '',
   );
 
   /// 네이버 Client ID.
   static const String naverClientId = String.fromEnvironment(
     'NAVER_CLIENT_ID',
-    defaultValue: 'LaLqTUwNHmjlcUpO4Mlv',
+    defaultValue: '',
   );
 
   /// 네이버 Client Secret.
@@ -39,6 +39,6 @@ class Env {
   /// 구글 Client ID.
   static const String googleClientId = String.fromEnvironment(
     'GOOGLE_CLIENT_ID',
-    defaultValue: '987643896621-4mr99pgantk7l6tq6quupq2bvkfiunp4.apps.googleusercontent.com',
+    defaultValue: '',
   );
 }
